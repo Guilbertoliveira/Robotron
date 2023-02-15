54e1ef1f11c4a395b2c171514d184dec54340462
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-02-14 21:04:09 -0300

    Feature enhancements and creations

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-const robotron = document.querySelector('#robotron');
-robotron.addEventListener('click', dizOi);
 
-
-function dizOi() {
-    console.log("oi");
+function subtrairElemento(elemento) {
+    return elemento.value = parseInt(elemento.value) - 1;
 }
+
+function somarElemento(elemento) {
+    return elemento.value = parseInt(elemento.value) + 1;
+}
+
+const subtrair = document.getElementById('subtrair');
+const somar = document.getElementById('somar');
+const braco = document.getElementById('braco');
+
+
+somar.addEventListener('click', ()=>{
+    braco.value = somarElemento(braco);
+})
+
+subtrair.addEventListener('click', ()=> {
+    braco.value = subtrairElemento(braco);
+})
