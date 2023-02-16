04342b27e8879fdef730db8d531a5c45bd2510f1
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-02-16 14:41:28 -0300

    update code for semantic improvements

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
-const controle = document.querySelectorAll('.controle-ajuste');
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+const controle = document.querySelectorAll('[data-controle]');
 
 
 function manipulaDados(operacao, controle){
-    const peca = controle.querySelector(".controle-contador");
+    const peca = controle.querySelector("[data-contador]");
 
     if(operacao === '-'){
         peca.value = peca.value = parseInt(peca.value) - 1;
@@ -17,8 +51,8 @@ function manipulaDados(operacao, controle){
 
 controle.forEach((elemento)=>{
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode);
-        console.log(evento.target.textContent, evento.target.parentNode);
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+        console.log(evento.target.dataset.controle);
         
     });
 });
